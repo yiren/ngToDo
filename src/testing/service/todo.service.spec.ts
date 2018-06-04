a1c38eeda703c667f08753d1ab4680fc268b575c
yiren <yiren.goo@gmail.com>
2018-06-04 21:57:20 +0800

    spec updated by 20180604

---
+++ src/testing/service/todo.service.spec.ts
@@ -0,0 +1,12 @@
+describe('Todo Service',()=>{
+  it('取得Todo list',()=>{
+
+  });
+
+  it('儲存ToDo',()=>{
+
+  });
+  it('更新Todo By Id',()=>{
+
+  })
+})
--- src/testing/store/ToDoActions.spec.ts
+++ src/testing/store/ToDoActions.spec.ts
@@ -3,33 +3,57 @@ import * as moment from 'moment';
 import {uuid} from 'uuid';
 
 describe('測試ToDo Actions', ()=>{
-  describe('新增ToDos Action', ()=>{
-    
-      it('包含載入Todo Action', ()=>{
-        const newTodoItem: ToDoItem={
-          todoId: uuid(),
-          description: '測試Action',
-          estimateDate: moment(),
-          estimateTime: moment(),
-          comment: 'Comment測試',
-          isCompleted: false,
-          isEdit: false,
-          isPrioritized: true,
-          itemFiles:[
-            {
-              fileId:1,
-              fileName:'20180514.zip',
-              uploadedDate:moment(),
-              filePath:'http://'
-            }
-          ]
-        };
-        const action = new AddToDosAction(newTodoItem);
+  describe('新增ToDo', ()=>{
+    const newTodoItem: ToDoItem={
+      todoId: uuid(),
+      description: '測試Action',
+      estimateDate: moment().toString(),
+      estimateTime: moment().toString(),
+      comment: 'Comment測試',
+      isCompleted: false,
+      isEdit: false,
+      isPrioritized: true,
+      itemFiles:[
+        {
+          fileId:1,
+          fileName:'20180514.zip',
+          uploadedDate:moment().toString(),
+          filePath:'http://'
+        }
+      ]
+    };
+      it('新增Todo Action', ()=>{
+        const action = new AddToDoAction(newTodoItem);
 
         expect({...action}).toEqual({
-          type: '[ToDo] Add ToDo',
+          type: '[ToDo] Add ToDo Action',
           payload: newTodoItem
         });
+      });
+      it('完成新增Todo Action', ()=>{
+          
+          const action = new AddToDoSuccessAction(newTodoItem);
+  
+          expect({...action}).toEqual({
+            type: '[ToDo] Add ToDo Success',
+            payload: newTodoItem
+          });
     });
   });
+
+  describe('更新Todo item',()=>{
+    
+    it('更新todoItem',()=>{
+
+    });
+
+    it('僅更新todo為Priority Item',()=>{
+
+    });
+
+    it('mark todoitem為complete',()=>{
+
+    });
+  });
+  
 });
