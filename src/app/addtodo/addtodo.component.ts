2d674835206215430ea3ecb4cd10b483aac9d0d6
yiren <yiren.goo@gmail.com>
2018-06-06 19:26:36 +0800

    考慮自己刻nav tab

--- src/app/addtodo/addtodo.component.ts
+++ src/app/addtodo/addtodo.component.ts
@@ -1,3 +1,4 @@
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
@@ -10,10 +11,26 @@ export class AddtodoComponent implements OnInit {
 
   constructor(private fb:FormBuilder) { }
 
+  
+  isPrioritized=false;
+  isEdit=false;
   todoForm:FormGroup;
-
+  
   ngOnInit() {
-    
+  }
+
+  enableEdit(){
+    this.isEdit=true;
+  }
+  hideEdit(){
+    this.isEdit=false;
+  }
+
+  togglePrioritize(){
+    this.isPrioritized = !this.isPrioritized;
+  }
+  cancel(){
+    this.isEdit=false;
   }
 
 }
--- src/app/todoitem/todoitem.component.ts
+++ src/app/todoitem/todoitem.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'f2e-todoitem',
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todoitem.component.scss']
 })
 export class TodoitemComponent implements OnInit {
+  
+  @Input()
+  isEdit:boolean;
+  
+  @Input()
+  isPrioritized:boolean;
 
+  @Input()
+  isComplete:boolean;
+
+  @Input()
+  hasFile:boolean;
+
+  @Input()
+  hasComment:boolean;
+
+  @Input()
+  deadline:{hasDeadline:boolean, date:string}
+  @Input()
+  text:string;
+
+  textAreaEditable = false;
   constructor() { }
-
+  
   ngOnInit() {
   }
+  enableTextAreaEdit(){
+    this.textAreaEditable=true;
+  }
 
+  togglePrioritize(){
+    this.isPrioritized=!this.isPrioritized;
+  }
+
+  toggleEdit(){
+    this.isEdit=!this.isEdit;
+  }
+  cancel(){
+    this.isEdit=false;
+  }
+  save(){
+    
+  }
+
+  
 }
